0653d9630efda5704f1d2903ea1e8696232fa3c2
mjali <mjali@ZOSH_70>
2023-08-02 09:04:31 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GeeksProject02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
