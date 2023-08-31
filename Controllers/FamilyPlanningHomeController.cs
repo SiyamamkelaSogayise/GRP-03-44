9cec9d46e9d272755235074bc0eee1f6369ddd3b
Mdekazi, Nkcubeko, (Mr) (s223005924) <s223005924@mandela.ac.za>
2023-08-31 12:48:55 +0200

    moja

---
+++ Controllers/FamilyPlanningHomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeeksProject02.Controllers
+{
+    public class FamilyPlanningHomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
