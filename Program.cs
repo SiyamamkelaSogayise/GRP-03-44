3144f4c5f7efaf4ad7deb70faeb2717bfcf66d66
Zenande N. S. Mjali <s224348906@mandela.ac.za>
2023-08-31 22:28:53 +0200

    Some issues with login but, i think it's sorted.

--- Program.cs
+++ Program.cs
@@ -39,13 +39,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapRazorPages();
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
-});
 app.UseAuthentication();;
 
 app.UseAuthorization();
