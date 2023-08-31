23a368d159b6f06e3763b2e8c563864440e88f25
Mjali, Zenande, (Mr) (s224348906) <s224348906@mandela.ac.za>
2023-08-31 09:05:48 +0200

    added my link to the main branch.

---
+++ Controllers/PrenatalController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeeksProject02.Controllers
+{
+    public class PrenatalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CheckUps()
+        {
+            return View();
+        }
+        public IActionResult PersonalInfo()
+        {
+            return View();
+        }
+        public IActionResult FeedBack()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        ////Get Method which returns the Create.cshtml
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        ////POST-Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Create(Fighters fighters)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Fighters.Add(fighters);
+        //        _context.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(fighters);
+        //}
+    }
+}
