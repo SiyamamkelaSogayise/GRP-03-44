d2229cdf3ae56da56ffbf89913bd86c05912e5df
Manti, Siphosethu, (Mr) (s224567306) <s224567306@mandela.ac.za>
2023-10-18 19:06:56 +0200

    a little function

---
+++ Models/BookingChronic.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace GeeksProject02.Models
+{
+    public class BookingChronic
+    {
+        [Key]
+        public int BookingID { get; set; }
+        
+        public string PreferredGenderConsultant { get; set; }
+        [Required]
+        public string BookingDate { get; set; }
+
+        [Required] 
+
+        public string BookingTime { get; set; }
+    }
+}
