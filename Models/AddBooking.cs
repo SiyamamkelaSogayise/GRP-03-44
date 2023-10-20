a3109959db45f750638f64991d45165ef88de458
Menzi-Thami <43884343+Menzi-Thami@users.noreply.github.com>
2023-10-20 14:20:06 +0200

    finished my admin page, working with DB now

---
+++ Models/AddBooking.cs
@@ -0,0 +1,27 @@
+﻿namespace GeeksProject02.Models
+{
+    public class AvailableDay
+    {
+        public int Id { get; set; }
+        public string Day { get; set; }
+    }
+
+    public class PreferredAppointmentTime
+    {
+        public int Id { get; set; }
+        public string Time { get; set; }
+    }
+    public class AddBooking
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string DOB { get; set; }
+        public string Gender { get; set; }
+        public List<AvailableDay>? AvailableDays { get; set; }
+        public List<PreferredAppointmentTime>? PreferredAppointmentTime { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public bool IsMedicalAidMember { get; set; }
+    }
+
+}
