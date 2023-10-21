3f6913da7870b3177147271da04f3a7ac9b32f92
Menzi-Thami <43884343+Menzi-Thami@users.noreply.github.com>
2023-10-21 10:06:32 +0200

    putting the AboutUs pictures back

---
+++ Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeeksProject02.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
