eaa0cb9c8233059eccc9b27cfb858d8e59919159
Menzi-Thami <43884343+Menzi-Thami@users.noreply.github.com>
2023-10-21 10:19:18 +0200

    Added Contact us page

---
+++ Models/ContactUsModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GeeksProject02.Models
+{
+    public class ContactUsModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+
+    }
+}
