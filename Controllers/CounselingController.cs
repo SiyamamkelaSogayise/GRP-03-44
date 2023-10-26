e729954ed17956d51fcb17f5fef7df362835c7aa
Maqondo, Avela, (Mr) (s224484575) <s224484575@mandela.ac.za>
2023-10-26 15:53:36 +0200

    Yeah

--- Controllers/CounselingController.cs
+++ Controllers/CounselingController.cs
@@ -71,6 +71,25 @@ namespace GeeksProject02.Controllers
         {
             return View();
         }
+        
 
+    [HttpGet]
+    public IActionResult Download()
+    {
+        // Path to your document
+        string filePath = "C:\\Users\\s224484575\\Source\\Repos\\GRP-03-44\\wwwroot\\css\\PDF Doc\\Meaningful Minds Psychologists - Mental Health Guide.pdf  "; // Update with the correct path
+
+        // Check if the file exists
+        if (System.IO.File.Exists(filePath))
+        {
+            // Serve the file for download
+            var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            return File(fileStream, "application/pdf", "your_document.pdf");
+        }
+        else
+        {
+            return NotFound(); // Or return an appropriate error response
+        }
+    }
     }
 }
